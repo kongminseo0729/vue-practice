1bc9786c3b141117a5dd08b5948e785c4b95d3c0
kongminseo0729 <kongminseo2002@naver.com>
2025-12-29 00:55:32 +0900

    게시판만들기1(헤더부분)

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup>
 //import button1 from './practice/button1.vue'
 //import todolist from './practice/todolist.vue'
-import todolistUpgrade from './practice/todolistUpgrade.vue'
+//import todolistUpgrade from './practice/todolistUpgrade.vue'
+import BoardList from './components/BoardList.vue'
 </script>
 
 <template>
 <!-- <button1 /> -->
 <!--<todolist />-->
-<todolistUpgrade/>
-
+<!-- <todolistUpgrade/> -->
+<BoardList/>
 </template>
---
+++ src/components/BoardList.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id="wrap">
+        <header id="header">ms의 게시판 목록</header>
+        <main id="main"></main>
+        <footer id="footer"></footer>
+    </div>
+</template>
+
+<style scoped>
+*{
+    margin:0;
+    padding:0;
+}
+#wrap{
+    width: 100%;
+}
+#header{
+    width: 100%;
+    height: 200px;
+    border-bottom:  5px solid #95c044;
+    font-family: 'GmarketSans', sans-serif;
+    font-size: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+#main{
+    width: 100%;
+    height: 600px;
+    background-color: #e2a9a9;
+}
+#footer{
+    width: 100%;
+    height: 100px;
+    background-color: #c3e2a9;
+}
+</style>
+
+<script setup>
+</script>
